ac0fb58f81d0838e57891a5f6afcd07908ae5329
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-03-22 23:15:36 -0400

    Image saving

---
+++ src/main/java/com/tmportfolio/recipeapp/controllers/ImageController.java
@@ -0,0 +1,37 @@
+package com.tmportfolio.recipeapp.controllers;
+
+import com.tmportfolio.recipeapp.services.ImageService;
+import com.tmportfolio.recipeapp.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class ImageController {
+
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+    public ImageController(ImageService imageService, RecipeService recipeService) {
+        this.imageService = imageService;
+        this.recipeService = recipeService;
+    }
+
+    @PostMapping("/recipe/{id}/image")
+    public String handleImagePost(@PathVariable String id, @RequestParam("imagefile") MultipartFile file){
+        imageService.saveImageFile(Long.valueOf(id), file);
+
+        return "redirect:/recipe/" + id + "/show";
+    }
+
+    @GetMapping("/recipe/{recipeId}/image")
+    public String showImageForm(@PathVariable String recipeId, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/imageUploadForm";
+    }
+}
