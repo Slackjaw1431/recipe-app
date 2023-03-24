50a7719cb8ff245f3dc935b465a5a103a18a173c
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-03-24 00:37:59 -0400

    Render image from DB

--- src/main/java/com/tmportfolio/recipeapp/controllers/ImageController.java
+++ src/main/java/com/tmportfolio/recipeapp/controllers/ImageController.java
@@ -1,7 +1,10 @@
 package com.tmportfolio.recipeapp.controllers;
 
+import com.tmportfolio.recipeapp.commands.RecipeCommand;
 import com.tmportfolio.recipeapp.services.ImageService;
 import com.tmportfolio.recipeapp.services.RecipeService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -33,5 +40,23 @@ public class ImageController {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 
         return "recipe/imageUploadForm";
+    }
+
+    @GetMapping("/recipe/{id}/recipeImage")
+    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+
+            for (Byte wrappedByte : recipeCommand.getImage()){
+                byteArray[i++] = wrappedByte; //auto unboxing
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
     }
 }
--- src/test/java/com/tmportfolio/recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/com/tmportfolio/recipeapp/controllers/ImageControllerTest.java
@@ -7,10 +7,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -61,6 +63,35 @@ public class ImageControllerTest {
                 .andExpect(model().attributeExists("recipe"));
 
         verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+
+    @Test
+    public void renderImageFromDB() throws Exception {
+
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        String s = "image test";
+        Byte[] bytesBoxed = new Byte[s.getBytes().length];
+
+        int i = 0;
+
+        for(byte primitiveByte : s.getBytes()){
+            bytesBoxed[i++] = primitiveByte;
+        }
+
+        command.setImage(bytesBoxed);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
+        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeImage"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        byte[] responseBytes = response.getContentAsByteArray();
+
+        assertEquals(s.getBytes().length, responseBytes.length);
+
     }
 
 }
